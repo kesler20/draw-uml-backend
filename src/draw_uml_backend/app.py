f5f7ed1c9e011c4f73e1fb7081978ef6cb301ed5
kesler20 <uchekesla@gmail.com>
2022-12-18 03:58:30 +0000

    compleed change to the server

--- src/draw_uml_backend/app.py
+++ src/draw_uml_backend/app.py
@@ -4,12 +4,7 @@ from starlette.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from draw_uml_backend.file import File
 from draw_uml_backend.routines import *
-from draw_uml_backend.class_builder import ClassBuilder
-from draw_uml_backend.code_reader import PythonCodeReader
-from draw_uml_backend.diagram_generator import DiagramGenerator
-from draw_uml_backend.test_builder import TestBuilder
-from draw_uml_backend.source_code import SourceCode
-from draw_uml_backend.types_pre_processing import TypeChecker
+
 # ---------------------------------------------------#
 #                                                    #
 #     INITIALIZE APPLICATION AND CONFIGURATIONS      #
@@ -40,31 +35,30 @@ async def read_root():
 #                                     #
 # ------------------------------------#
 
-
-@app.get('/python_file')
+@app.get('/get/python/code')
 async def handle_get_python_file():
     print("get python file called")
-    return FileResponse("file.py", media_type="text/x-python", filename=code_output_file)
+    return FileResponse(code_output_file, media_type="text/x-python", filename=code_output_file)
 
-@app.get('/python_test_file/io')
+@app.get('/get/python/test/io')
 async def handle_get_python_test_file():
     print("get python test file called")
-    return FileResponse("test_file.py", media_type="text/x-python", filename=test_file_path_io)
+    return FileResponse(test_file_path_io, media_type="text/x-python", filename=test_file_path_io)
 
-@app.get('/python_test_file/side_effects')
+@app.get('/get/python/test/side_effects')
 async def handle_get_python_test_file():
     print("get python test file called")
-    return FileResponse("test_file.py", media_type="text/x-python", filename=test_file_path_side_effects)
+    return FileResponse(test_file_path_side_effects, media_type="text/x-python", filename=test_file_path_side_effects)
 
-@app.get('/design_document')
+@app.get('/get/markdown/diagram')
 async def handle_get_javascript_file():
     print("get design document file called")
-    return FileResponse("file.js", media_type="text/x-markdown", filename="file.js")
+    return FileResponse(documentation_path, media_type="text/x-markdown", filename=documentation_path)
 
-@app.get('/python_types_file')
+@app.get('/get/python/types')
 async def handle_get_javascript_file():
     print("get types file called")
-    return FileResponse("file.js", media_type="text/x-python", filename=types_file)
+    return FileResponse(types_file, media_type="text/x-python", filename=types_file)
 
 @app.post('/create/new/python/files/dataclass')
 async def handle_diagram_data(diagram=Body(...)):
