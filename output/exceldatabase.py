82fc6d8d5e06d3485b438766f1aafeba1deb9154
kesler20 <uchekesla@gmail.com>
2023-03-20 11:26:40 +0000

    chore: make it better🪴

--- output/exceldatabase.py
+++ output/exceldatabase.py
@@ -1,7 +1,7 @@
 
 from typing import List, Any, Union, Dict, Optional, Tuple
 from dataclasses import dataclass, field
-from output._types import DataFrame, str
+from output._types import str, DataFrame
         
 @dataclass
 class ExcelDatabase:
